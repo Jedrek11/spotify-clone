fc97d46f7be06141d2e97892d153bd7458ac4769
Jedrek11 <jedrekpawlowski011@gmail.com>
2024-05-30 15:50:46 +0200

    Klon Spotify - Play Song in Player (start i stop player'a muzyki)

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,28 +1,35 @@
-import React from "react";
-import { assets, songsData } from "../assets/assets";
+import React, { useContext } from "react";
+import { assets } from "../assets/assets";
+import { PlayerContext } from "../context/PlayerContext";
 
 const Player = () => {
+
+    const {track, seekBar, seekBg, playStatus, play, pause} = useContext(PlayerContext);
+
   return (
     <div className="h-[10%] bg-black flex justify-between items-center text-white px-4">
         <div className="hidden lg:flex items-center gap-4">
-            <img className="w-12" src={songsData[0].image} alt="" />
+            <img className="w-12" src={track.image} alt="" />
             <div>
-                <p>{songsData[0].name}</p>
-                <p>{songsData[0].desc.slice(0-12)}</p>
+                <p>{track.name}</p>
+                <p>{track.desc.slice(0-12)}</p>
             </div>
         </div>
         <div className="flex flex-col items-center gap-1 m-auto">
             <div className="flex gap-4">
-                 <img className="w-4 cursor-pointer" src={assets.shuffle_icon} alt="" /> {/* Na zmiane piosenka */}
-                <img className="w-4 cursor-pointer" src={assets.prev_icon} alt="" /> {/* Poprzednia piosenka */}
-                <img className="w-4 cursor-pointer" src={assets.play_icon} alt="" /> {/* Puść Piosenkę */}
-                <img className="w-4 cursor-pointer" src={assets.next_icon} alt="" /> {/* Następna piosenka */}
-                <img className="w-4 cursor-pointer" src={assets.loop_icon} alt="" /> {/* Na okrągło piosenka */}
+                 <img className="w-4 cursor-pointer" src={assets.shuffle_icon} alt="shuffle" /> {/* Na zmiane piosenka */}
+                <img className="w-4 cursor-pointer" src={assets.prev_icon} alt="previuse" /> {/* Poprzednia piosenka */}
+                {playStatus
+                ?<img onClick={pause} className="w-4 cursor-pointer" src={assets.pause_icon} alt="stop" />
+                :<img onClick={play} className="w-4 cursor-pointer" src={assets.play_icon} alt="start" />
+                }                     
+                <img className="w-4 cursor-pointer" src={assets.next_icon} alt="next" /> {/* Następna piosenka */}
+                <img className="w-4 cursor-pointer" src={assets.loop_icon} alt="loop" /> {/* Na okrągło piosenka */}
             </div>
             <div className="flex items-center gap-5">
                 <p>1:06</p>
-                <div className="w-[60vw] max-w-[500px] bg-gray-300 rounded-full cursor-pointer">
-                    <hr className="h-1 border-none w-20 bg-green-800 rounded-full" />
+                <div ref={seekBg} className="w-[60vw] max-w-[500px] bg-gray-300 rounded-full cursor-pointer">
+                    <hr ref={seekBar} className="h-1 border-none w-20 bg-green-800 rounded-full" />
                 </div>
                 <p>3:20</p>
             </div>
